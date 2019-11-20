ba50d4b822547749c951e0b4e2dfb0f392d00c61
rishabhguptarishi <rishabh@vinsol.com>
2019-11-20 13:31:28 +0530

    Exercise1

--- Exercise1/LibraryQueries.sql
+++ Exercise1/LibraryQueries.sql
@@ -1,25 +1,51 @@
 Select Title
 From Titles
-Where Publisher Like 'Macmilian';
+Where Publisher = 'Macmilian';
+/*+----------+
+| Title    |
++----------+
+| Susannah |
+| The Wife |
++----------+
+2 rows in set (0.00 sec)*/
 
-Select Distinct branches.*
-From branches
-inner join holdings on branches.BCode = holdings.Branch
-inner join titles on holdings.Title = titles.Title
-Where titles.Author Like 'Ann Brown';
-
-Select *
-From branches
-where BCode in(
-Select Branch
+Select Distinct holdings.Branch
 From holdings
-Where Title in(
-Select Title
-From Titles
-Where Author Like 'Ann Brown'
-)
+Inner Join titles on holdings.Title = titles.Title
+Where titles.Author = 'Ann Brown';
+/*+--------+
+| Branch |
++--------+
+| B1     |
+| B3     |
+| B2     |
++--------+
+3 rows in set (0.00 sec)*/
+
+Select Distinct Branch
+From holdings
+Where Title IN (
+  Select Title
+  From Titles
+  Where Author = 'Ann Brown'
 );
+/*+--------+
+| Branch |
++--------+
+| B1     |
+| B3     |
+| B2     |
++--------+
+3 rows in set (0.00 sec)*/
 
 Select Branch, sum(Number_of_copies)
 From Holdings
-Group By Branch
+Group By Branch;
+/*+--------+-----------------------+
+| Branch | sum(Number_of_copies) |
++--------+-----------------------+
+| B1     |                     6 |
+| B2     |                     9 |
+| B3     |                     9 |
++--------+-----------------------+
+3 rows in set (0.12 sec)*/
--- Exercise1/SandwhichQueries.sql
+++ Exercise1/SandwhichQueries.sql
@@ -1,17 +1,44 @@
-Select locatiOn
+use learnings;
+Select location
 From sandwiches
-Where Filling in(
+Where Filling IN (
   Select Filling
   From tastes
   Where Name = 'Jones'
 );
+/*
++-----------+
+| location  |
++-----------+
+| Buttery   |
+| O'Neill's |
++-----------+
+2 rows in set (0.00 sec) */
 
 Select sandwiches.location
 From sandwiches
 Inner Join tastes On sandwiches.Filling = tastes.Filling
 Where tastes.name = 'Jones';
+/*
++-----------+
+| location  |
++-----------+
+| Buttery   |
+| O'Neill's |
++-----------+
+2 rows in set (0.00 sec)  */
 
-Select sandwiches.location, count(distinct tastes.Name)
+Select sandwiches.location, count(distinct tastes.Name) noOfPeople
 From sandwiches
 Inner Join tastes On sandwiches.Filling = tastes.Filling
-Group By sandwiches.location
+Group By sandwiches.location;
+/*
++-----------+------------+
+| location  | noOfPeople |
++-----------+------------+
+| Buttery   |          3 |
+| Lincon    |          2 |
+| O'Neill's |          3 |
+| Old Nag   |          2 |
++-----------+------------+
+4 rows in set (0.00 sec) */

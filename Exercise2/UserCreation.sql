3606b380b2376d5305c4b1cf774c8947fcd406c0
rishabhguptarishi <rishabh@vinsol.com>
2019-11-20 13:52:28 +0530

    UserCreation

--- Exercise2/UserCreation.sql
+++ Exercise2/UserCreation.sql
@@ -3,7 +3,30 @@ Create Database vtapp;
  Create User vtapp_user@localhost Identified By 'password';
  
 select user from mysql.user;
+/*+------------------+
+| user             |
++------------------+
+| ODBC             |
+| bob              |
+| mysql.infoschema |
+| mysql.session    |
+| mysql.sys        |
+| rishi            |
+| root             |
+| vtapp_user       |
++------------------+
+8 rows in set (0.12 sec)*/
 
 Grant ALL
 On vtapp.*
 To vtapp_user@localhost;
+/*Query OK, 0 rows affected (1.26 sec)*/
+
+show grants for vtapp_user@localhost;
+/*+---------------------------------------------------------------+
+| Grants for vtapp_user@localhost                               |
++---------------------------------------------------------------+
+| GRANT USAGE ON *.* TO `vtapp_user`@`localhost`                |
+| GRANT ALL PRIVILEGES ON `vtapp`.* TO `vtapp_user`@`localhost` |
++---------------------------------------------------------------+
+2 rows in set (0.00 sec)*/

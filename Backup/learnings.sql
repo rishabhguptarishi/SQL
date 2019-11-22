226b3d9cca127ab87a46213b3736823be1bed18f
rishabhguptarishi <rishabh@vinsol.com>
2019-11-22 14:18:57 +0530

    Backup and restored

---
+++ Backup/learnings.sql
@@ -0,0 +1,205 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: learnings
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `branches`
+--
+
+DROP TABLE IF EXISTS `branches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `branches` (
+  `BCode` varchar(3) NOT NULL,
+  `Librarian` varchar(255) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`BCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `branches`
+--
+
+LOCK TABLES `branches` WRITE;
+/*!40000 ALTER TABLE `branches` DISABLE KEYS */;
+INSERT INTO `branches` VALUES ('B1','John Smith','2 Anglesea Rd'),('B2','Mary Jones','34 Pearse St'),('B3','Francis Owens','Grange X');
+/*!40000 ALTER TABLE `branches` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `holdings`
+--
+
+DROP TABLE IF EXISTS `holdings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `holdings` (
+  `Branch` varchar(3) NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `Number_of_copies` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Branch`,`Title`),
+  KEY `fk_title` (`Title`),
+  CONSTRAINT `fk_branch` FOREIGN KEY (`Branch`) REFERENCES `branches` (`BCode`),
+  CONSTRAINT `fk_title` FOREIGN KEY (`Title`) REFERENCES `titles` (`Title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `holdings`
+--
+
+LOCK TABLES `holdings` WRITE;
+/*!40000 ALTER TABLE `holdings` DISABLE KEYS */;
+INSERT INTO `holdings` VALUES ('B1','History of Dublin',1),('B1','How to fish',2),('B1','Susannah',3),('B2','Computers',2),('B2','How to fish',4),('B2','The Wife',3),('B3','Computers',4),('B3','History of Dublin',1),('B3','Susannah',3),('B3','The Wife',1);
+/*!40000 ALTER TABLE `holdings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `locations` (
+  `LName` varchar(255) NOT NULL,
+  `Phone` varchar(10) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  PRIMARY KEY (`LName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locations`
+--
+
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` VALUES ('Buttery','7023421','College St'),('Lincon','6834523','Lincon Place'),('O\'Neill\'s','6742134','Pearse St'),('Old Nag','7678132','Dame St');
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sandwiches`
+--
+
+DROP TABLE IF EXISTS `sandwiches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sandwiches` (
+  `Location` varchar(255) NOT NULL,
+  `Bread` varchar(255) NOT NULL,
+  `Filling` varchar(255) NOT NULL,
+  `Price` double(10,2) NOT NULL,
+  PRIMARY KEY (`Location`,`Bread`,`Filling`),
+  CONSTRAINT `fk_location` FOREIGN KEY (`Location`) REFERENCES `locations` (`LName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sandwiches`
+--
+
+LOCK TABLES `sandwiches` WRITE;
+/*!40000 ALTER TABLE `sandwiches` DISABLE KEYS */;
+INSERT INTO `sandwiches` VALUES ('Buttery','White','Cheese',1.00),('Buttery','White','Ham',1.10),('Lincon','Rye','Beef',1.35),('Lincon','Rye','Ham',1.25),('Lincon','White','Ham',1.40),('O\'Neill\'s','White','Cheese',1.20),('O\'Neill\'s','White','Turkey',1.25),('O\'Neill\'s','Whole','Ham',1.35),('Old Nag','Rye','Beef',1.35),('Old Nag','Rye','Ham',1.40);
+/*!40000 ALTER TABLE `sandwiches` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tastes`
+--
+
+DROP TABLE IF EXISTS `tastes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tastes` (
+  `Name` varchar(255) NOT NULL,
+  `Filling` varchar(255) NOT NULL,
+  PRIMARY KEY (`Name`,`Filling`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tastes`
+--
+
+LOCK TABLES `tastes` WRITE;
+/*!40000 ALTER TABLE `tastes` DISABLE KEYS */;
+INSERT INTO `tastes` VALUES ('Brown','Beef'),('Brown','Ham'),('Brown','Turkey'),('Green','Beef'),('Green','Cheese'),('Green','Turkey'),('Jones','Cheese');
+/*!40000 ALTER TABLE `tastes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `testing_table`
+--
+
+DROP TABLE IF EXISTS `testing_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `testing_table` (
+  `username` varchar(255) DEFAULT NULL,
+  `roll_no` int(11) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `testing_table`
+--
+
+LOCK TABLES `testing_table` WRITE;
+/*!40000 ALTER TABLE `testing_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `testing_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `titles`
+--
+
+DROP TABLE IF EXISTS `titles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `titles` (
+  `Title` varchar(255) NOT NULL,
+  `Author` varchar(255) DEFAULT NULL,
+  `Publisher` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `titles`
+--
+
+LOCK TABLES `titles` WRITE;
+/*!40000 ALTER TABLE `titles` DISABLE KEYS */;
+INSERT INTO `titles` VALUES ('Computers','Blaise Pascal','Applewoods'),('History of Dublin','David Little','Wiley'),('How to fish','Amy Fly','Stop Press'),('Susannah','Ann Brown','Macmilian'),('The Wife','Ann Brown','Macmilian');
+/*!40000 ALTER TABLE `titles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-22 14:02:55
